5669fbcfe73b45a9c3d1e3b25239c37dfc8736c2
jinsyin <jinsyin@gmail.com>
2017-12-21 15:05:16 +0800

    install k8s node components

---
+++ kubernetes/install-k8s-node.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Author: JinsYin <github.com/jinsyin>
+
+set -e
+
+K8S_VERSION="1.8.2"
+
+fn::check_permission()
+{
+  if [ $(id -u) -ne 0 ]; then
+    echo "You must run as root user or through the sudo command."
+    exit 1
+  fi
+}
+
+fn::command_exists() 
+{
+  command -v $@ > /dev/null 2>&1
+}
+
+fn::package_exists()
+{
+  rpm -q $@ > /dev/null 2>&1
+}
+
+# Usage: fn::instasll_package wget net-tools
+fn::install_package()
+{
+  for package in $@; do
+    if ! fn::package_exists $package; then
+      yum install -y $package
+    fi
+  done
+}
+
+fn::install_k8s_node()
+{
+  local version=v${1:-$K8S_VERSION}
+  local components=(kubectl kube-proxy kubelet)
+
+  fn::install_package wget
+
+  for component in ${components[@]}; do
+    if ! fn::command_exists ${component}; then
+      rm -rf /tmp/k8s-server* && mkdir -p /tmp/k8s-server
+      wget -O /tmp/k8s-server.tar.gz https://dl.k8s.io/${version}/kubernetes-server-linux-amd64.tar.gz
+      tar -zxf /tmp/k8s-server.tar.gz -C /tmp/k8s-server --strip-components=1
+      cp -r /tmp/k8s-server/server/bin/{kubectl,kube-proxy,kubelet} /usr/bin/
+      rm -rf /tmp/k8s-server*
+    fi
+  done
+}
+
+main()
+{
+  fn::check_permission
+  fn::install_k8s_node $@
+}
+
+main $@
